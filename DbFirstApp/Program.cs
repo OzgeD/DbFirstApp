b3db252819e563780a98a06c05130a8d763ffee1
OzgeD <OzgeD@users.noreply.github.com>
2020-10-11 00:43:24 +0300

    Adding basic CRUD operations

--- DbFirstApp/Program.cs
+++ DbFirstApp/Program.cs
@@ -11,13 +11,44 @@ namespace DbFirstApp
         static void Main(string[] args)
         {
             NORTHWNDEntities db = new NORTHWNDEntities();
+            ////CRUD
+            ////Create
+            //var category = new Category();
+            //category.CategoryName = "Çerezler";
+            //category.Description = "Fındık,Fıstık,Ceviz...";
 
-            var sales = db.Sales_by_Year(new DateTime(1997, 1, 1),DateTime.Now);
-            foreach (var sale in sales)
-            {
-                Console.WriteLine(sale.Year + "/" + sale.Subtotal);
-            }
+            //db.Categories.Add(category);
+            //db.SaveChanges();
+            //Console.ReadLine();
+
+            ////Read
+            //var categories = db.Categories;
+            //foreach(var cat in categories)
+            //{
+            //    Console.WriteLine(cat.CategoryName + "/" + cat.Description);
+            //}
+            //Console.ReadLine();
+
+            ////Update
+            //var category = db.Categories.Find(10);
+            //category.CategoryName = "İçecekler";
+            //category.Description = "Su,Kola,Meyve suyu...";
+            //db.SaveChanges();
+            //Console.ReadLine();
+
+            //Delete
+            var category = db.Categories.Find(10);
+            db.Categories.Remove(category);
+            db.SaveChanges();
             Console.ReadLine();
+
+            ///Modele stored procedure dahil etme
+            //var sales = db.Sales_by_Year(new DateTime(1997, 1, 1),DateTime.Now);
+            //foreach (var sale in sales)
+            //{
+            //    Console.WriteLine(sale.Year + "/" + sale.Subtotal);
+            //}
+            //Console.ReadLine();
 
             //var products = db.Ten_Most_Expensive_Products();
             //foreach(var pro in products)
