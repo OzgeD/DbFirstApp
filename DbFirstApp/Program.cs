79fae129da6e70186a399d0dc65fcf2e2f8e2bff
OzgeD <OzgeD@users.noreply.github.com>
2020-10-12 00:24:48 +0300

    Learn difference between Immediate Mode and Defered Mode

--- DbFirstApp/Program.cs
+++ DbFirstApp/Program.cs
@@ -13,42 +13,16 @@ namespace DbFirstApp
         static void Main(string[] args)
         {
             NORTHWNDEntities db = new NORTHWNDEntities();
-            var result = (from p in db.Products
-                         join c in db.Categories
-                         on p.CategoryID equals c.CategoryID
-                         group new { c,p } by new { c.CategoryName } into g
-                         select new
-                         {
-                             g.Key.CategoryName,
-                             NumberOfProducts=g.Count(),
-                             TotalPrice = g.Sum(x=>x.p.UnitPrice),
-                             MaxPrice = g.Max(x=>x.p.UnitPrice),
-                             MinPrice = g.Min(x => x.p.UnitPrice)
-                         }).Where(x=>x.TotalPrice>300);
+            //Immediate Mode vs Defered Mode
+            //Immediate'da kodun çağrılmasıyla beraber sql profiler'a sorgu gidiyor
+            //var categories = db.Categories.ToList(); //Immediate Mode
 
-            //var result = from p in db.Products
-            //             group p by p.CategoryID into g
-            //             select new
-            //             {
-            //                 g.Key,
-            //                 NumberOfProducts = g.Count()
-            //             };
+            //Defered Mode'da ise kodun çağrılmasıyla sql profiler'a sorgu gitmiyor,
+            //categories'e ihtiyaç duyulup kullanıldığında sql profiler'a sorgu gidiyor.
+            var categories = db.Categories; //Defered Mode
+            categories.Count();
 
-            //var result = from c in db.Categories
-            //             join p in db.Products
-            //             on c.CategoryID equals p.ProductID
-            //             orderby p.UnitPrice descending
-            //             select new
-            //             {
-            //                 p.ProductName,
-            //                 c.CategoryName,
-            //                 p.UnitPrice
-            //             };
-
-
-            ConsoleTable.From(result).Write();
             Console.ReadLine();
-
         }
     }
 }
